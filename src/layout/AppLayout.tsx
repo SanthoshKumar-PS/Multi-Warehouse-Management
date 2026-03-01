f0d43a31ceaee933def737039f618e763335e0ec
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2026-03-01 09:45:50 +0530

    Updated applayout with max-w

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -5,8 +5,10 @@ const AppLayout = () => {
   return (
     <div className="flex min-h-screen flex-col">
         <Navbar/>
-        <main className='flex-1 flex flex-col overflow-auto p-2 md:p-4 lg:p-6'>
-            <Outlet/>
+        <main className="flex-1 flex flex-col p-2 md:p-4 lg:p-6 overflow-x-hidden">
+          <div className="w-full max-w-7xl mx-auto flex-1 flex flex-col">
+            <Outlet />
+          </div>
         </main>
 
     </div>
